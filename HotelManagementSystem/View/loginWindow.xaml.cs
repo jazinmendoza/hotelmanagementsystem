816c4e5b07c2dfbfc7edb8c7268718056130a6b8
Marian Isabel <yaneemozo@gmail.com>
2018-02-01 10:08:24 +0800

    Add files via upload
    ayaw ni idelete

---
+++ HotelManagementSystem/View/loginWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using HotelManagementSystem.ViewModel;
+using HotelManagementSystem.Helper;
+
+namespace HotelManagementSystem.View
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class loginWindow : Window
+    {
+        public loginWindow()
+        {
+            InitializeComponent();
+            DataContext = new LoginViewModel();
+        }
+
+        // Passes user inputs to LoginViewModel (function: loginUser).
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginViewModel a = new LoginViewModel();
+            a.loginUser(username.Text, password.Text);
+        }
+    }
+}
