dc4436b06762007ce77d6d22ed24e59924bed430
Kent Michael Talisaysay <tkentmichaell@gmail.com>
2018-02-17 10:48:31 +0800

    Add files via upload

---
+++ View/GuestList.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelManagementSystem.View
+{
+    /// <summary>
+    /// Interaction logic for GuestList.xaml
+    /// </summary>
+    public partial class GuestList : Window
+    {
+        public GuestList()
+        {
+            InitializeComponent();
+        }
+
+        private void backBtn_Clicked(object sender, MouseButtonEventArgs e)
+        {
+            this.Visibility = System.Windows.Visibility.Collapsed;
+            Dashboard a = new Dashboard();
+            a.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
---
+++ View/RoomMonitoring.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using HotelManagementSystem.View.FloorPages;
+
+namespace HotelManagementSystem.View
+{
+    /// <summary>
+    /// Interaction logic for RoomMonitoring.xaml
+    /// </summary>
+    public partial class RoomMonitoring : Window
+    {
+        public RoomMonitoring()
+        {
+            InitializeComponent();
+        }
+
+        private void backBtn_Clicked(object sender, MouseButtonEventArgs e)
+        {
+            this.Visibility = System.Windows.Visibility.Collapsed;
+            Dashboard a = new Dashboard();
+            a.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void firstBtn_Clicked(object sender, RoutedEventArgs e)
+        {
+            floorPage_Frame.Content = new FirstFloorView();
+        }
+
+        private void secondBtn_Clicked(object sender, RoutedEventArgs e)
+        {
+            floorPage_Frame.Content = new SecondFloorView();
+        }
+
+        private void thirdBtn_Clicked(object sender, RoutedEventArgs e)
+        {
+            floorPage_Frame.Content = new ThirdFloorView();
+        }
+
+        private void fourthBtn_Clicked(object sender, RoutedEventArgs e)
+        {
+            floorPage_Frame.Content = new FourthFloorView();
+        }
+
+
+    }
+}
