3772d168ac92e12bb2e4d424ce80e33cff0037e1
jazinmendoza <jazinmendoza16@gmail.com>
2018-02-24 15:05:20 +0800

    Add files via upload

---
+++ Backup/REST.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="REST.asmx.cs" Class="WebService.REST" %>
---
+++ Backup/REST.asmx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WebService
+{
+    /// <summary>
+    /// Summary description for REST
+    /// </summary>
+    [WebService(Namespace = "http://kidscape.com/webservices/", Description="This is a web service for Kidscape", Name="RestService")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class REST : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string GetCustomerName(int id)
+        {
+            //get customer name from the database.
+            return "Microsoft Corporation";
+        }
+
+        [WebMethod]
+        public bool InsertCustomer(int id, string customerName)
+        {
+
+            return true;
+        }
+        [WebMethod]
+        public DataTable connectoToMySql()
+        {
+            string connString = "SERVER=localhost" + ";" +
+                "DATABASE=sample;" +
+                "UID=root;" +
+                "PASSWORD=;";
+
+            MySqlConnection cnMySQL = new MySqlConnection(connString);
+
+            MySqlCommand cmdMySQL = cnMySQL.CreateCommand();
+
+            MySqlDataReader reader;
+
+            cmdMySQL.CommandText = "select * from cities";
+
+            cnMySQL.Open();
+
+            reader = cmdMySQL.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+
+
+            cnMySQL.Close();
+
+            return dt;
+        } 
+    }
+}
