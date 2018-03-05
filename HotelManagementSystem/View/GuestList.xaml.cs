8ed508078397e7b150dca8a66be78ebf5814050c
Kent Michael Talisaysay <tkentmichaell@gmail.com>
2018-03-05 23:24:23 +0800

    Add files via upload

--- HotelManagementSystem/View/GuestList.xaml.cs
+++ HotelManagementSystem/View/GuestList.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace HotelManagementSystem.View
 {
@@ -23,7 +24,7 @@ namespace HotelManagementSystem.View
         {
             InitializeComponent();
             ServiceReference1.RestServiceSoapClient a = new ServiceReference1.RestServiceSoapClient();
-            GuestListDataGrid.DataContext = a.connectoToMySql().DefaultView;
+            GuestListDataGrid.DataContext = a.getGuestList().DefaultView;
         }
 
         private void backBtn_Clicked(object sender, MouseButtonEventArgs e)
@@ -36,6 +37,37 @@ namespace HotelManagementSystem.View
         private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void Button_ClickSearch(object sender, RoutedEventArgs e)
+        {
+            ServiceReference1.RestServiceSoapClient search = new ServiceReference1.RestServiceSoapClient();
+            GuestListDataGrid.DataContext = search.getDataFromSearch(searchBox.Text).DefaultView;
+        }
+
+        private void viewBillingBtnClicked(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = System.Windows.Visibility.Collapsed;
+            Billing a = new Billing();
+            a.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void checkOutBtnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GuestListDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            string guestId = GuestListDataGrid.SelectedItem.ToString();
+            //string fname = GuestListDataGrid.CurrentRow.Cells[1].Value.ToString();
+            //string lname = GuestListDataGrid.CurrentRow.Cells[2].Value.ToString();
+           // string guestStatus = GuestListDataGrid.CurrentRow.Cells[6].Value.ToString();
+
+            fName.Content = guestId;
+            //lName.Content = lname;
+            //status.Content = guestStatus;
         }
 
         
